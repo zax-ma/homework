689a66d4ea7526a03fb37884a1b2a7406b204aac
Maria Zakharevich <zaxarevich_ma@yahoo.com>
2022-08-06 10:56:12 +0300

    ДЗ1 java

---
+++ src/home_work_1/BitOperator.java
@@ -0,0 +1,31 @@
+package home_work_1;
+
+import java.util.Scanner;
+
+public class BitOperator {
+    public static void main(String[] args) {
+    Scanner console = new Scanner(System.in);
+    System.out.println("Введите первое число:");
+    int a = console.nextInt();
+    System.out.println("Введите второе число:");
+    int b = console.nextInt();
+    System.out.println("1: "+Integer.toBinaryString(a));
+    System.out.println("2: "+Integer.toBinaryString(b));
+    System.out.println("Побитовый И с двумя числами: "+Integer.toBinaryString(a&b));
+    /* пример: 548(1000100100) и 777(1100001001)
+    1000100100
+    &
+    1100001001
+    -----------
+    1000000000
+     */
+    System.out.println("Побитовый ИЛИ с двумя числами: "+Integer.toBinaryString(a|b));
+        /* пример: 548(1000100100) и 777(1100001001)
+    1000100100
+    |
+    1100001001
+    -----------
+    1100101101
+         */
+}
+}
