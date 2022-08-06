804b3cd91366e73a4c3930a7a49fcefecabf456b
Maria Zakharevich <zaxarevich_ma@yahoo.com>
2022-08-06 18:56:18 +0300

    ДЗ1 4.2_Среди трёх чисел найти среднее

--- src/home_work_1/Average.java
+++ src/home_work_1/Average.java
@@ -5,17 +5,15 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        double frstNum;
-        double scndNum;
-        double thrdNum;
+
         System.out.println("Введите первое число:");
-        frstNum = console.nextDouble();
+        double frstNum = console.nextDouble();
 
         System.out.println("Введите второе число:");
-        scndNum = console.nextDouble();
+        double scndNum = console.nextDouble();
 
         System.out.println("Введите третье число:");
-        thrdNum = console.nextDouble();
+        double thrdNum = console.nextDouble();
 
         double average = (frstNum + scndNum + thrdNum)/3.0;
         if (average == (int) average) {
