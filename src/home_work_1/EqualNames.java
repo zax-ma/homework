647d702280dd6c953e5c83cabaaf405ec77f8de4
Maria Zakharevich <zaxarevich_ma@yahoo.com>
2022-08-08 16:16:24 +0300

    package home_work_1;
    import java.util.Objects;
    import java.util.Scanner;
    
    public class EqualNames {
    
        public static void main(String[] args) {
            String vasya = "Вася";
            String nastya = "Анастасия";
    
            System.out.println(vasya);
            Scanner console = new Scanner(System.in);
            System.out.println("Введите Ваше имя:");
            String name = console.nextLine();
            if (Objects.equals(name, vasya)) {
    
                System.out.println("Привет!\nЯ тебя так долго ждал!");
            } else if (Objects.equals(name, nastya)) {
                System.out.println("Привет!\nЯ тебя так долго ждал!");
    
            } else {
                System.out.println("Добрый день, а вы кто?");
            }
        }
    }

---
+++ src/home_work_1/EqualNames.java
@@ -0,0 +1,26 @@
+package home_work_1;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class EqualNames {
+
+    public static void main(String[] args) {
+        String vasya = "Вася";
+        String nastya = "Анастасия";
+
+        System.out.println(vasya);
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите Ваше имя:");
+        String name = console.nextLine();
+        if (Objects.equals(name, vasya)) {
+
+            System.out.println("Привет!\nЯ тебя так долго ждал!");
+        } else if (Objects.equals(name, nastya)) {
+            System.out.println("Привет!\nЯ тебя так долго ждал!");
+
+        } else {
+            System.out.println("Добрый день, а вы кто?");
+        }
+    }
+}
---
+++ src/home_work_1/PhoneNumber.java
@@ -0,0 +1,29 @@
+package home_work_1;
+
+import java.util.Scanner;
+
+public class PhoneNumber {
+    public static void main(String[] args) {
+        System.out.println("Введите 10 цифр номера телефона:");
+        Scanner console = new Scanner(System.in);
+        char[] line = console.next().trim().toCharArray();
+        if (line.length != 10) {
+            System.out.println("Проверьте корректность номера");
+        } else {
+        String format = createPhoneNumber(line);
+        System.out.println(format);
+        }
+    }
+    public static String createPhoneNumber(char[] linex) {
+        String result = "(";
+        for (int i = 0; i < linex.length; i++) {
+            result += linex[i];
+            if (i == 2){
+                result += ") ";
+            } else if (i == 5) {
+                result += "-";
+            }
+        }
+        return result;
+    }
+}
