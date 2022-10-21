7a67176389c39182c601488e925cbd970ec3ee22
Maria Zakharevich <zaxarevich_ma@yahoo.com>
2022-10-21 23:04:54 +0300

    main

--- src/warandpeace/searchers/EasySearch.java
+++ src/warandpeace/searchers/EasySearch.java
@@ -3,6 +3,7 @@ package warandpeace.searchers;
 import warandpeace.api.ISearchEngine;
 
 public class EasySearch implements ISearchEngine {
+    private static final String splitter = "-*[](),;:.?! ";
 
     static long wordCounter = 0;
 
@@ -12,6 +13,12 @@ public class EasySearch implements ISearchEngine {
         return wordCounter;
     }
 
+    /**
+     * Простой поиск слова в строке без учета пунктуации
+     * @param text - текст, в котором нужно найти слово
+     * @param word - слово
+     * @return сколько раз в тексте встречается искомое слово
+     */
     public long search(String text, String word) {
         name = word;
         if (word == "") {
@@ -23,20 +30,40 @@ public class EasySearch implements ISearchEngine {
 
             }
             return 0;
-        }else if (word.length() <=3 && word.length()>0){
-            word = " " + word + " ";
         }
+            int count = 0;
+            for (int i = 0; i < text.length(); i++) {
+                int wordFirstIndex = word.indexOf(text.charAt(i));
+                int wordLength = word.length();
 
+                if ((wordFirstIndex > -1 && wordFirstIndex != 0)
+                        && (splitter.indexOf(text.charAt(i - 1)) > -1)
+                        && (splitter.indexOf(text.charAt(i + wordLength)) > -1)
+                        && (i + wordLength < text.length() - 1)) {
+                    for (int j = 0; j < wordLength; j++) {
+                        if (wordFirstIndex > -1) {
+                            count++;
+                        }
+                    }
+                } else if (wordFirstIndex == 0
+                        && i + wordLength < text.length()
+                        && (splitter.indexOf(text.charAt(i + wordLength)) > -1)) {
+                    for (int j = 0; j < wordLength; j++) {
+                        if (wordFirstIndex > -1) {
+                            count++;
+                        }
+                    }
+                } else if (wordFirstIndex > -1 && i + wordLength == text.length() - 2) {
+                    for (int j = 0; j < wordLength; j++) {
+                        if (wordFirstIndex > -1) {
+                            count++;
+                        }
+                    }
+                }
+            }
 
-        int position = text.indexOf(word);
-        while (position != -1) {
-            wordCounter++;
-            position = text.indexOf(word, position + 1);
-
+            return wordCounter = count / word.length();
         }
-
-        return wordCounter;
-    }
 
     @Override
     public String toString () {
