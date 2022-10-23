bf4172af34ff6b6612ce7bd7a7c92b3e61d69bac
Maria Zakharevich <zaxarevich_ma@yahoo.com>
2022-10-23 16:31:51 +0300

    new EasySearch

--- src/warandpeace/searchers/EasySearch.java
+++ src/warandpeace/searchers/EasySearch.java
@@ -2,8 +2,11 @@ package warandpeace.searchers;
 
 import warandpeace.api.ISearchEngine;
 
+import java.util.PrimitiveIterator;
+
 public class EasySearch implements ISearchEngine {
-    private static final String splitter = "-*[](),;:.?! ";
+    private static final String splitter = ",;:.?! \"\n";
+
 
     static long wordCounter = 0;
 
@@ -31,39 +34,71 @@ public class EasySearch implements ISearchEngine {
             }
             return 0;
         }
-            int count = 0;
-            for (int i = 0; i < text.length(); i++) {
-                int wordFirstIndex = word.indexOf(text.charAt(i));
-                int wordLength = word.length();
+        int count = 0;
+        String lowerCaseTextString = text.toLowerCase();
+        String lowerCaseWord = word.toLowerCase();
+        int wordLength = word.length();
 
-                if ((wordFirstIndex > -1 && wordFirstIndex != 0)
-                        && (splitter.indexOf(text.charAt(i - 1)) > -1)
-                        && (splitter.indexOf(text.charAt(i + wordLength)) > -1)
-                        && (i + wordLength < text.length() - 1)) {
-                    for (int j = 0; j < wordLength; j++) {
-                        if (wordFirstIndex > -1) {
-                            count++;
-                        }
-                    }
-                } else if (wordFirstIndex == 0
-                        && i + wordLength < text.length()
-                        && (splitter.indexOf(text.charAt(i + wordLength)) > -1)) {
-                    for (int j = 0; j < wordLength; j++) {
-                        if (wordFirstIndex > -1) {
-                            count++;
-                        }
-                    }
-                } else if (wordFirstIndex > -1 && i + wordLength == text.length() - 2) {
-                    for (int j = 0; j < wordLength; j++) {
-                        if (wordFirstIndex > -1) {
-                            count++;
-                        }
+        for (int i = 0; i < text.length(); i++) {
+            int wordFirstIndex = word.indexOf(text.charAt(i));
+
+            //если текст состоит из одного искомого слова
+            if (wordLength == text.length()) {
+                for (int j = 0; j < wordLength; j++) {
+                    if (word.charAt(j) == text.charAt(i)) {
+                        count++;
+                        i++;
                     }
                 }
             }
 
-            return wordCounter = count / word.length();
+            //если слово стоит в начале строки
+            else if(i == 0 && wordFirstIndex == 0
+                    && (splitter.indexOf(text.charAt(i + wordLength)) > -1)){
+                for (int j = 0; j < wordLength; j++) {
+                    if (word.charAt(j) == text.charAt(i)) {
+                        count++;
+                        i++;
+                    }
+                }
+            }
+
+            //если слово стоит в середине строки
+            else if ( i > 0 && wordFirstIndex == 0
+                    && i + wordLength < text.length()
+                    && (splitter.indexOf(text.charAt(i - 1)) > -1)
+                    && (splitter.indexOf(text.charAt(i + wordLength)) > -1)) {
+                for (int j = 0; j < wordLength; j++) {
+                    if (word.charAt(j) == text.charAt(i)) {
+                        count++;
+                        i++;
+                    }
+                }
+            }
+
+            //если слово стоит в конце строки
+            else if (i > 0 && i + wordLength == text.length()
+                    && ((splitter.indexOf(text.charAt(i - 1)) > -1)
+                    || (splitter.indexOf(text.charAt(i - 1)) > -1))) {
+                for (int j = 0; j < wordLength; j++) {
+                    if (word.charAt(j) == text.charAt(i)) {
+                        count++;
+                        i++;
+                    }
+                }
+            }
+
+            if (count % wordLength != 0) {
+                while (count % wordLength != 0) {
+                    count--;
+                }
+
+            }
+
+
         }
+        return wordCounter = count / wordLength;
+    }
 
     @Override
     public String toString () {
